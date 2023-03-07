aa159e269bea821ca9913943da9d443dea8898bf
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-03-07 10:53:26 +0800

    Initial commit

---
+++ command_line.sh
@@ -0,0 +1,21 @@
+#for create project card with version laravel 8.0
+composer create-project laravel/laravel card
+
+
+@extends('name') 
+# sama denganinclude di php
+
+@yield('name')
+# Menyelipkan codingan
+
+@section('name')
+@endsection
+# manggil yield
+
+
+@stack
+#memasukan bagian dari laman child
+
+@push('')
+@endpush
+# isi code laman child untuk di push ke parent
---
+++ resources/views/index.blade.php
@@ -0,0 +1,1 @@
+<h5>Welcome to index</h5>
